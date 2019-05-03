172c8aa4a1316d46d39be72867ccce01ed2efae1
nikcoding <nikulagrawal@gmail.com>
2019-05-03 12:36:30 +0530

    1 commit

---
+++ Bot1.java
@@ -0,0 +1,105 @@
+package my;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+
+public class Bot1 {
+
+	public static void main(String[] args) throws IOException, AWTException, InterruptedException
+	{
+	String command="notepad.exe";
+	Runtime run=Runtime.getRuntime();
+	run.exec(command);
+	try {
+		
+	}
+
+	catch(Exception e)
+	{
+		
+	}
+	Robot robot=new Robot();
+	robot.keyPress(KeyEvent.VK_H);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_E);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_L);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_L);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_O);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_SPACE);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_N);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_I);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_K);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_U);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_L);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_SPACE);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_T);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_H);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_I);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_S);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_SPACE);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_I);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_S);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_SPACE);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_A);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_SPACE);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_B);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_O);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_T);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_SPACE);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_M);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_A);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_D);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_E);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_SPACE);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_B);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_Y);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_SPACE);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_J);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_A);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_V);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_A);
+	Thread.sleep(300);
+	robot.keyPress(KeyEvent.VK_SPACE);
+	
+	}
+
+}
